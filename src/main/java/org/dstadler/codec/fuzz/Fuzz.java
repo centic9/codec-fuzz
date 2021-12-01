278eebd771520493048ce148c92a3dc508b9cfe0
Dominik Stadler <dominik.stadler@gmx.at>
2021-12-01 16:23:33 +0100

    Fix mapping int to enum and add handling of IllegalArgumentException

--- src/main/java/org/dstadler/codec/fuzz/Fuzz.java
+++ src/main/java/org/dstadler/codec/fuzz/Fuzz.java
@@ -16,18 +16,19 @@ import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 
 /**
  * This class provides a simple target for fuzzing Apache Commons Codec with Jazzer.
- *
+ * <p>
  * It uses the fuzzed input data to try to base64 encode/decode data.
- *
+ * <p>
  * It catches all exceptions that are currently expected.
  */
 public class Fuzz {
+
 	public static void fuzzerTestOneInput(FuzzedDataProvider data) {
 		int lineLength = data.consumeInt();
 		byte[] lineSeparator = data.consumeBytes(10);
 		boolean useHex = data.consumeBoolean();
 		byte bytePadding = data.consumeByte();
-		CodecPolicy policy = CodecPolicy.values()[data.consumeInt(0, CodecPolicy.values().length)];
+		CodecPolicy policy = CodecPolicy.values()[data.consumeInt(0, CodecPolicy.values().length-1)];
 		String hex = data.consumeString(10);
 
 		byte[] bytes = data.consumeRemainingAsBytes();
@@ -45,9 +46,18 @@ public class Fuzz {
 			// expected here
 		}
 
-		Base32 base32 = new Base32(lineLength, lineSeparator, useHex, bytePadding);
-		base32.decode(bytes);
-		base32.encode(bytes);
+		try {
+			Base32 base32 = new Base32(lineLength, lineSeparator, useHex, bytePadding);
+			base32.decode(bytes);
+		} catch (IllegalArgumentException e) {
+			// expected here
+		}
+		try {
+			Base32 base32 = new Base32(lineLength, lineSeparator, useHex, bytePadding);
+			base32.encode(bytes);
+		} catch (IllegalArgumentException e) {
+			// expected here
+		}
 
 		// try to invoke various methods which read archive data
 		try {
@@ -57,9 +67,18 @@ public class Fuzz {
 			// expected here
 		}
 
-		Base64 base64 = new Base64(lineLength, lineSeparator, useHex, policy);
-		base64.decode(bytes);
-		base64.encode(bytes);
+		try {
+			Base64 base64 = new Base64(lineLength, lineSeparator, useHex, policy);
+			base64.decode(bytes);
+		} catch (IllegalArgumentException e) {
+			// expected here
+		}
+		try {
+			Base64 base64 = new Base64(lineLength, lineSeparator, useHex, policy);
+			base64.encode(bytes);
+		} catch (IllegalArgumentException e) {
+			// expected here
+		}
 
 		try {
 			Hex.decodeHex(Hex.encodeHex(bytes));
